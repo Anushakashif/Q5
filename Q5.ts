029ce2c220de38de8508892cb6392311ad100fe3
Anusha Kashif <160410652+Anushakashif@users.noreply.github.com>
2024-03-21 14:40:37 -0700

    Add files via upload

---
+++ Q5.ts
@@ -0,0 +1,7 @@
+//  5. Famous Quote : Repeat Exercise 4, but this time store the famous person's name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person: string = "William shakespeare"
+
+let message: string = "Brevity is the soul of wit"
+
+console.log(` ${famous_person} Once said,  “${message}” `)
